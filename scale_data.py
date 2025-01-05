dcb47720680d804ba2b34aa2e1ad0d6043c31406
ifxtars <l200220180@student.ums.ac.id>
2025-01-05 08:19:02 +0700

    rename false file name

---
+++ scale_data.py
@@ -0,0 +1,18 @@
+import tarfile
+from itertools import islice
+
+def load_yelp_reviews(num_docs):
+    f = 'wa_guyup_rukun_sak_lawase_clean.tar'
+    with tarfile.open(f) as tar:
+            print('Read tar file ... ')
+            datafile = tar.extractfile('wa_guyup_rukun_sak_lawase_clean/wa_guyup_rukun_sak_lawase_clean.csv')
+            return list(islice(datafile, num_docs))
+
+def make_matrix(docs, binary=False):
+    from sklearn.feature_extraction.text import CountVectorizer
+    vec = CountVectorizer(min_df=10, max_df=0.1, binary=binary)
+    mtx = vec.fit_transform(docs)
+    cols = [None] * len(vec.vocabulary_)
+    for word, idx in vec.vocabulary_.items():
+        cols[idx] = word
+    return mtx, cols
